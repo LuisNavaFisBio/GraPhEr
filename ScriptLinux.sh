5778c2b075a37b31047a1afaba19bc37934390bd
Luis Enrique Nava Garcia <135725561+LuisNavaFisBio@users.noreply.github.com>
2025-01-03 20:24:52 -0600

    Archivos para automatizar la ejecucion en Linux

---
+++ ScriptLinux.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo apt-get install python3-pip ffmpeg python3-venv && python3 -m venv grapher_env \
+source "./grapher_env/bin/activate" 
+pip install -U --only-binary :all: matplotlib==3.7.5 PyQt5 plasTeX sympy scipy PySide6 ffmpeg-python pyqtwebengine && deactivate 
